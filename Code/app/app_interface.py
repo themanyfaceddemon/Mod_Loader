81cf088bcf7629e92622e8974fb12e5dd074377c
themanyfaceddemon <neko-d-05@mail.ru>
2024-11-05 17:44:30 +0300

    some fun

--- Code/app/app_interface.py
+++ Code/app/app_interface.py
@@ -1,9 +1,13 @@
+import logging
+import os
+import subprocess
+import sys
 import dearpygui.dearpygui as dpg
-
+from Code.package import ModLoader, Package
 import Code.dpg_tools as dpg_tools
 from Code.app_vars import AppGlobalsAndConfig
 from Code.loc import Localization as loc
-
+import platform
 from .barotrauma_window import BarotraumaWindow
 from .mod_window import ModWindow
 
@@ -27,10 +31,23 @@ class AppInterface:
             dpg.add_button(
                 label=loc.get_string("btn-set-game-dir"),
                 callback=BarotraumaWindow.create_window,
-                tag="set_dir_button",
             )
-            with dpg.tooltip("set_dir_button"):
+            with dpg.tooltip(dpg.last_item()):
                 dpg.add_text(loc.get_string("btn-set-game-dir-desc"))
+
+            dpg.add_checkbox(
+                label=loc.get_string("setting-toggle-install-lua"),
+                tag="settings_install_lua",
+                default_value=AppGlobalsAndConfig.get("game_config_auto_lua", False),  # type: ignore
+                callback=AppInterface.enable_auto_install_lua,
+            )
+
+            dpg.add_checkbox(
+                label=loc.get_string("setting-toggle-skip-intro"),
+                tag="settings_skip_intro",
+                default_value=AppGlobalsAndConfig.get("game_config_skip_intro", False),  # type: ignore
+                callback=AppInterface.enable_auto_skip_intro,
+            )
 
             dpg.add_checkbox(
                 label=loc.get_string("menu-toggle-experimental"),
@@ -46,6 +63,12 @@ class AppInterface:
                 ),  # TODO: Dynamic language replacement
                 width=50,
             )
+
+        dpg.add_menu_item(
+            label=loc.get_string("menu-bar-start-game"),
+            parent="main_view_bar",
+            callback=AppInterface.start_game,
+        )
 
     @staticmethod
     def resize_windows():
@@ -72,3 +95,99 @@ class AppInterface:
         if dpg.does_item_exist("exp_game"):
             dpg.configure_item("exp_game", width=viewport_width, height=viewport_height)
             dpg_tools.center_window("exp_game")
+
+        if dpg.does_item_exist("game_config_window"):
+            dpg.configure_item(
+                "game_config_window", width=viewport_width, height=viewport_height
+            )
+            dpg_tools.center_window("game_config_window")
+
+    @staticmethod
+    def enable_auto_install_lua(sender, app_data, user_data):
+        if app_data:
+            AppGlobalsAndConfig.set("game_config_auto_lua", True)
+            AppGlobalsAndConfig.set("game_config_skip_intro", False)
+            dpg.set_value("settings_skip_intro", False)
+        else:
+            AppGlobalsAndConfig.set("game_config_auto_lua", False)
+
+    @staticmethod
+    def enable_auto_skip_intro(sender, app_data, user_data):
+        if app_data:
+            AppGlobalsAndConfig.set("game_config_skip_intro", True)
+            AppGlobalsAndConfig.set("game_config_auto_lua", False)
+            dpg.set_value("settings_install_lua", False)
+        else:
+            AppGlobalsAndConfig.set("game_config_skip_intro", False)
+
+    @staticmethod
+    def start_game():
+        ModLoader.save_mods()
+
+        game_dir = AppGlobalsAndConfig.get("barotrauma_dir", None)
+        if not game_dir:
+            AppInterface.show_error("Game dir not set!")
+            return
+
+        skip_intro = AppGlobalsAndConfig.get("game_config_skip_intro", False)
+        auto_install_lua = AppGlobalsAndConfig.get("game_config_auto_lua", False)
+
+        if auto_install_lua:
+            command = AppInterface.get_auto_install_command()
+            if command:
+                AppInterface.execute_command(command, game_dir)
+        else:
+            AppInterface.run_game(skip_intro, game_dir)
+
+    @staticmethod
+    def show_error(message):
+        with dpg.window(label="error"):
+            dpg.add_text(message)
+
+    @staticmethod
+    def get_auto_install_command():
+        system = platform.system()
+        if system == "Windows":
+            return (
+                'cmd /c "curl -L -z Luatrauma.AutoUpdater.win-x64.exe -o Luatrauma.AutoUpdater.win-x64.exe '
+                "https://github.com/Luatrauma/Luatrauma.AutoUpdater/releases/download/latest/Luatrauma.AutoUpdater.win-x64.exe && "
+                'start /b Luatrauma.AutoUpdater.win-x64.exe"'
+            )
+        elif system == "Darwin":
+            return (
+                '/bin/zsh -c "cd Barotrauma.app/Contents/MacOS && '
+                "/usr/bin/curl -L -O https://github.com/Luatrauma/Luatrauma.AutoUpdater/releases/download/latest/Luatrauma.AutoUpdater.osx-x64 && "
+                'chmod +x Luatrauma.AutoUpdater.osx-x64 && ./Luatrauma.AutoUpdater.osx-x64"'
+            )
+        elif system == "Linux":
+            return (
+                'bash -c "wget https://github.com/Luatrauma/Luatrauma.AutoUpdater/releases/download/latest/Luatrauma.AutoUpdater.linux-x64 && '
+                'chmod +x Luatrauma.AutoUpdater.linux-x64 && ./Luatrauma.AutoUpdater.linux-x64"'
+            )
+        else:
+            AppInterface.show_error("Unknown operating system")
+            return None
+
+    @staticmethod
+    def execute_command(command, cwd):
+        try:
+            subprocess.run(command, shell=True, cwd=cwd)
+        except Exception as e:
+            logging.error(f"Error executing command: {e}")
+
+    @staticmethod
+    def run_game(skip_intro, game_dir):
+        parms = " -skipintro" if skip_intro else ""
+        system = platform.system()
+
+        if system == "Windows":
+            run = "Barotrauma.exe"
+        elif system == "Darwin":
+            run = "Barotrauma.app"
+        elif system == "Linux":
+            run = "Barotrauma"
+        else:
+            AppInterface.show_error("Unknown operating system")
+            return
+
+        subprocess.run([sys.executable, f"{game_dir}/{run}{parms}"], cwd=game_dir)
