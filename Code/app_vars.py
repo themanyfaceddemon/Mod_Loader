d054887559cfc270d7945e51a52c880e20c96fda
themanyfaceddemon <neko-d-05@mail.ru>
2024-11-19 14:17:07 +0300

    Transferring user configuration to a folder outside the program's working directory

--- Code/app_vars.py
+++ Code/app_vars.py
@@ -1,6 +1,7 @@
 import atexit
 import json
 import logging
+import platform
 from pathlib import Path
 from typing import Any, Dict, Optional
 
@@ -10,11 +11,33 @@ class AppConfig:
 
     _root: Path = Path(__file__).parents[1]
     _data_root: Path = _root / "Data"
-    _user_config_path: Path = _data_root / "user_config.json"
+    _user_config_path: Path = Path()
 
     @classmethod
-    def init(cls) -> None:
+    def init(cls, debug=False) -> None:
+        if platform.system() == "Windows":
+            AppConfig._user_config_path = (
+                Path.home()
+                / "AppData"
+                / "Roaming"
+                / "BarotraumaModdingTool"
+                / "config.json"
+            )
+        elif platform.system() == "Linux":
+            AppConfig._user_config_path = (
+                Path.home() / ".config" / "BarotraumaModdingTool" / "config.json"
+            )
+        elif platform.system() == "Darwin":
+            AppConfig._user_config_path = (
+                Path.home()
+                / "Library"
+                / "Application Support"
+                / "BarotraumaModdingTool"
+                / "config.json"
+            )
+
         cls._load_user_config()
+        cls.set("debug", debug)
         atexit.register(cls._save_user_config)
 
     @classmethod
@@ -30,6 +53,8 @@ class AppConfig:
     @classmethod
     def _save_user_config(cls) -> None:
         cls.user_config.pop("debug")
+
+        cls._user_config_path.mkdir(parents=True, exist_ok=True)
 
         with open(cls._user_config_path, "w", encoding="utf-8") as file:
             json.dump(cls.user_config, file, indent=4, sort_keys=True)
